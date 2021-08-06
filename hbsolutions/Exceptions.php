1693230caa2d729964c991d571b31c2a8fdbf8dd
hbdeveloper <programador@hotelesbuenaventura.com.mx>
2021-08-06 17:42:47 -0500

    Se agrego clase HttpExceptions

--- hbsolutions/Exceptions.php
+++ hbsolutions/Exceptions.php
@@ -1,9 +1,10 @@
 <?php
 namespace Hbsolutions;
 
-class Exceptions {
+class ExcepetionsHbsolutions extends \Exception{
 
-    public static function exceptionMessage(\Exception $e){
+    public static function exceptionFormat(\Exception $e){
+
         return [
             "message" => $e->getMessage(),
             "line" => $e->getLine(),
@@ -11,4 +12,32 @@ class Exceptions {
             "messageComplete" => $e->getTraceAsString()
         ];
     }
+}
+
+
+
+class HttpExceptions extends \Exception {
+
+    private $message;
+    private $data;
+    private $microservice;
+
+    public function __construct(String $message, String $data, String $microservice ){
+        $this->message = $message;
+        $this->data = $data;
+        $this->microservice = $microservice;
+    }
+
+
+    public function obtenerMensaje(){
+        return [
+            "message" => $this->message,
+            "data" => $this->data,
+            "microservice" => $this->microservice,
+        ];
+    }
+
+
+
+
 }
